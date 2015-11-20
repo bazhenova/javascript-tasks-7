0da61b5e4a3b7aded6a6d15befff5137aabca534
bazhenova <anyusha2595@gmail.com>
2015-11-21 02:05:13 +0500

    Еще null остался

--- src/check.js
+++ src/check.js
@@ -2,119 +2,91 @@
 
 exports.init = function () {
     Object.defineProperty(Object.prototype, 'check', {
-        get: getBindingMethods
+        get: function () {
+            var context = this;
+            var methods = getBindingMethods(this);
+            Object.defineProperty(methods, 'not', {
+                get: function () {
+                    var notMethods = getBindingMethods(context);
+                    var functions = Object.keys(notMethods);
+                    for (var i = 0; i < functions.length; i++) {
+                        notMethods[functions[i]] = function () {
+                            return !notMethods[functions[i]];
+                        }
+                    }
+                    return notMethods;
+                }
+            });
+            return methods;
+        }
     });
 };
 
-function getBindingMethods() {
-    var allMethods = getMethods();
+function getBindingMethods(context) {
+    var allMethods = getMethods(context);
     var methods = Object.keys(allMethods);
     for (var i = 0; i < methods.length; i++) {
-        allMethods[methods[i]] = allMethods[methods[i]].bind(this);
+        allMethods[methods[i]] = allMethods[methods[i]].bind(context);
     }
     return allMethods;
 }
 
-function getMethods() {
-    return {
-        containsKeys: function (keys) {
-            if (isRelevantPrototype(this, Array.prototype, Object.prototype)) {
-                for (var i = 0; i < keys.length; i++) {
-                    if (this.indexOf(keys[i]) === -1) {
-                        return false;
-                    }
-                }
-                return true;
-            }
-            return undefined;
-        },
+function getMethods(context) {
+    if (Object.getPrototypeOf(context) === Array.prototype ||
+        Object.getPrototypeOf(context) === Object.prototype) {
+        return {
+            containsKeys: function (keys) {
+                return keys.every(key => Object.keys(context).indexOf(key) !== -1);
+            },
 
-        hasKeys: function (keys) {
-            if (isRelevantPrototype(this, Array.prototype, Object.prototype)) {
-                var thisKeys = Object.keys(this);
-                for (var i = 0; i < thisKeys.length; i++) {
-                    if (keys.indexOf(thisKeys[i]) === -1) {
-                        return false;
-                    }
-                }
-                return true;
-            }
-            return undefined;
-        },
+            hasKeys: function (keys) {
+                return Object.keys(context).every(key => keys.indexOf(key) !== -1);
+            },
 
-        containsValues: function (values) {
-            if (isRelevantPrototype(this, Array.prototype, Object.prototype)) {
-                for (var i = 0; i < values.length; i++) {
-                    if (this.indexOf(values[i]) === -1) {
-                        return false;
-                    }
-                }
-                return true;
-            }
-            return undefined;
-        },
+            containsValues: function (values) {
+                return values.every(value => context.indexOf(value) !== -1);
+            },
 
-        hasValues: function (values) {
-            if (isRelevantPrototype(this, Array.prototype, Object.prototype)) {
-                var thisKeys = Object.keys(this);
-                for (var key in thisKeys) {
-                    if (values.indexOf(this[key]) === -1) {
-                        return false;
-                    }
-                }
-                return true;
-            }
-            return undefined;
-        },
+            hasValues: function (values) {
+                return Object.keys(context).every(key => values.indexOf(context[key]) !== -1);
+            },
 
-        hasValueType: function (key, type) {
-            if (isRelevantPrototype(this, Array.prototype, Object.prototype)) {
+            hasValueType: function (key, type) {
                 var types = ['string', 'number', 'function', 'array'];
-                return types.indexOf(typeof type()) !== -1 && typeof this[key] === typeof type();
-            }
-            return undefined;
-        },
+                return types.indexOf(typeof type()) !== -1 && typeof context[key] === typeof type();
+            },
 
-        hasLength: function (length) {
-            if (isRelevantPrototype(this, Array.prototype, String.prototype)) {
-                return this.length === length;
+            hasLength: function (length) {
+                if (Object.getPrototypeOf(context) === Array.prototype) {
+                    return context.length === length;
+                }
+                return undefined;
             }
-            return undefined;
-        },
-
-        hasParamsCount: function (count) {
-            if (isRelevantPrototype(this, Function.prototype)) {
-                return this.length === count;
+        };
+    } else if (Object.getPrototypeOf(context) === Function.prototype) {
+        return {
+            hasParamsCount: function (count) {
+                return context.length === count;
             }
-            return undefined;
-        },
-
-        hasWordsCount: function (count) {
-            if (isRelevantPrototype(this, String.prototype)) {
+        };
+    } else if (Object.getPrototypeOf(context) === String.prototype) {
+        return {
+            hasWordsCount: function (count) {
+                var words = context.split(/\s/);
                 var wordsCount = 0;
-                var exist = false;
-                for (var i = 0; i < this.length; i++) {
-                    if (this[i] === ' ' || i === this.length - 1) {
-                        if (exist) {
-                            wordsCount += 1;
-                            exist = false;
-                        }
-                    } else {
-                        exist = true;
+                for (var i = 0; i < words.length; i++) {
+                    if (words[i].length > 0) {
+                        wordsCount += 1;
                     }
                 }
                 return wordsCount === count;
-            }
-            return undefined;
-        }
-    };
-}
+            },
 
-function isRelevantPrototype(source) {
-    for (var i = 1; i < arguments.length; i++) {
-        if (Object.getPrototypeOf(source) === arguments[i]) {
-            return true;
-        }
+            hasLength: function (length) {
+                return context.length === length;
+            }
+        };
+    } else {
+        return undefined;
     }
-    return false;
 }
